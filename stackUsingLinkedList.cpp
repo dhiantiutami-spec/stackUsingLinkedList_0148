52056d9e8b107943a948a7eb2f11afae7e7fa380
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-05-14 11:23:12 +0700

    menambahkan menu keluar program

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -113,6 +113,10 @@ int main()
                 cout << "Stack is empty.No top value." << endl;
             }
             break;
+
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
         }
     }
 }
